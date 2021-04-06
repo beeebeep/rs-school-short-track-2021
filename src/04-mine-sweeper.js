67db85ee1a005b680e9987fdb602969f8a8a89bc
beeebeep <YouCanWriteMe@ya.ru>
2021-04-07 04:06:55 +0700

    feat: implement task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,36 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function markAround(res, origin) {
+  const pos = { X: origin.X, Y: origin.Y };
+  pos.X--;
+  pos.Y--;
+  for (let step = 0; step < 8; step++) {
+    if (pos.X >= 0 && pos.X < res[0].length && pos.Y >= 0 && pos.Y < res.length) {
+      res[pos.Y][pos.X] += 1;
+    }
+    let deltaX;
+    let deltaY;
+    switch (step) {
+      case 0: case 1: deltaX = 1; deltaY = 0; break;
+      case 2: case 3: deltaX = 0; deltaY = 1; break;
+      case 4: case 5: deltaX = -1; deltaY = 0; break;
+      case 6: case 7: deltaX = 0; deltaY = -1; break;
+      default: break;
+    }
+
+    pos.X += deltaX;
+    pos.Y += deltaY;
+  }
+}
+
+function minesweeper(matrix) {
+  const res = [...Array(matrix.length)].map(() => Array(matrix[0].length).fill(0));
+
+  matrix.forEach((arr, y) => arr.forEach((v, x) => {
+    if (matrix[y][x]) markAround(res, { X: x, Y: y });
+  }));
+  return res;
 }
 
 module.exports = minesweeper;

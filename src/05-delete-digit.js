2906a53bcb4320fb5773d2e53d0097a1a2fba2d8
beeebeep <YouCanWriteMe@ya.ru>
2021-04-07 22:28:57 +0700

    feat: implement tasks 05-06

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let max = -Infinity;
+  const str = n.toString();
+  [...str].forEach((_, i) => {
+    max = Math.max(max, Number(str.substring(0, i) + str.substring(i + 1)));
+  });
+  return max;
 }
 
 module.exports = deleteDigit;
